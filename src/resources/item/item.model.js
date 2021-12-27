4ae588f3272c1e7b84784d71ac9bae490f73dd6b
Sam <sam@Sams-MacBook-Pro-2.local>
2021-12-27 13:57:47 +0000

    definign shcemas lesson

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -13,8 +13,22 @@ const itemSchema = new mongoose.Schema(
       required: true,
       enum: ['active', 'complete', 'pastdue'],
       default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'list'
     }
   },
   { timestamps: true }
 )
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
